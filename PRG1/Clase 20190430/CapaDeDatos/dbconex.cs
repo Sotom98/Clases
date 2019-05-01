831345f56f24cf01f88fea4a2a40f78b3d9c41fa
Sotom98 <48165847+Sotom98@users.noreply.github.com>
2019-04-30 19:59:32 -0600

    20190430

---
+++ PRG1/Clase 20190430/CapaDeDatos/dbconex.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data.Sql;
+
+namespace CapaDeDatos
+{
+    public class dbconex
+    {
+        string cadenaConex = "Data Source=.;Initial Catalog=BaseDePruebas;Persist Security Info=True;User ID=Sotom98;Password=sql";
+
+        public bool conex()
+        {
+            bool correcto = false;
+            try {
+                correcto = true;
+
+                catch(Exception ex) {
+                    correcto = false;
+                }
+            }            
+
+            return correcto;
+        }
+    }
+}
---
+++ PRG1/Clase 20190430/CapaDeNegocios/mantVndrs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDeDatos;
+
+namespace CapaDeNegocios
+{
+    public class mantVndrs
+    {
+        dbconex cntc = new dbconex();
+    }
+}
